abfb8f198149e6ffb962817342beebdd53312992
Willy Fortuna <willyfortunasc.unb@gmail.com>
2025-11-10 09:14:14 -0300

    tarefas1

---
+++ tarefas/tarefas_app/admin.py
@@ -0,0 +1,9 @@
+# Register your models here.
+from django.contrib import admin
+from .models import Tarefa
+
+@admin.register(Tarefa)
+class TarefaAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'concluida', 'data_criacao')
+    list_filter = ('concluida', 'data_criacao')
+    search_fields = ('titulo', 'descricao')
