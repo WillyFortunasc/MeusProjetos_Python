6f5213ff0ef0d2fe114eebc7f8169976813caca1
Willy Fortuna <willyfortunasc.unb@gmail.com>
2025-11-14 23:12:45 -0300

    Delete Avaliação_3/rest directory

--- Avaliação_3/rest/repositories/icliente_repository.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC
-from models.cliente import Cliente
-from repositories.igeneric_repository import IGenericRepository
-
-class IClienteRepository(IGenericRepository[Cliente], ABC):
-    pass
--- Avaliação_3/rest/repositories/ipedido_repository.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC
-from models.pedido import Pedido
-from repositories.igeneric_repository import IGenericRepository
-
-class IPedidoRepository(IGenericRepository[Pedido], ABC):
-    pass
