4d3a09fc5ae3f26756199565ef60cc5c8f0d8813
Willy Fortuna <willyfortunasc.unb@gmail.com>
2025-11-14 23:16:00 -0300

    Avaliação_3

--- Rest_willy.pedidos/IWS/rest/models/cliente.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from .base import Base
-
-class Cliente(Base):
-    __tablename__ = 'cliente'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    nome = Column(String(100))
-    idade = Column(Integer)
-     
--- Rest_willy.pedidos/IWS/rest/repositories/cliente_repository.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.orm import Session
-from models.cliente import Cliente
-from repositories.icliente_repository import IClienteRepository
-from repositories.generic_repository import GenericRepository
-
-class ClienteRepository(GenericRepository[Cliente], IClienteRepository):
-    def __init__(self, db: Session):
-        super().__init__(db, Cliente)
